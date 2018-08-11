1e4851c7191b26152a2cc301418300085d85e459
Madhur Maheshwari <maheshwari.madhur@gmail.com>
2018-08-11 18:14:52 +0530

    Create HTTPWebserviceTest.java
    <dependency>
                <groupId>org.apache.httpcomponents</groupId>
                <artifactId>httpclient</artifactId>
                <version>4.5</version>
            </dependency>
            <dependency>
                <groupId>commons-logging</groupId>
                <artifactId>commons-logging</artifactId>
                <version>1.2</version>
            </dependency>

---
+++ src/main/java/com/home/madhur/HTTPWebserviceTest.java
@@ -0,0 +1,25 @@
+package com.home;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HTTPWebserviceTest {
+    public static void main(String[] args) throws ClientProtocolException, IOException {
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet("http://www.webservicex.net/stockquote.asmx?op=GetQuote");
+        HttpResponse response = client.execute(request);
+        BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+        String line = null;
+        while ((line = rd.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+}
